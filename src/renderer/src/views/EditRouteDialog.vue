4b3297fd9445b93c5e61ac875afa225b003128ef
Edsionss <bigsea1017@gmail.com>
2025-08-20 19:24:33 +0800

    refactor: 移除未使用的导入
    移除了 EditRouteDialog.vue 文件中未使用的 defineEmits 导入

--- src/renderer/src/views/EditRouteDialog.vue
+++ src/renderer/src/views/EditRouteDialog.vue
@@ -29,7 +29,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch, defineEmits } from 'vue'
+import { ref, watch } from 'vue'
 import { RouteRecord } from '@/stores/routesStore'
 
 const props = defineProps<{
