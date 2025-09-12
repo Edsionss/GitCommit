a7c79da29629adb83dc6649b245c4c77db918623
Edsionss <bigsea1017@gmail.com>
2025-09-12 23:34:52 +0800

    feat(components): 新增自定义 Tabs 组件并替换 MarketNewsflash 中的 ant-design Tabs
    - 新增自定义 Tabs 组件，位于 src/renderer/src/components/Common/Tabs.vue
    - 在 MarketNewsflash 组件中使用自定义 Tabs 组件替换原有的 ant-design Tabs
    - 自定义 Tabs 组件支持垂直标签页功能，并优化了样式
    - 更新 MarketNewsflash 组件的模板和样式，以适应新的自定义 Tabs 组件

--- src/renderer/src/components/stock/MarketNewsflash/index.vue
+++ src/renderer/src/components/stock/MarketNewsflash/index.vue
@@ -1,18 +1,36 @@
 <template>
-  <a-tabs v-model:activeKey="newsKey" tab-position="left" animated>
-    <a-tab-pane key="telegraph" tab="财联社">
-      <TimePanel :data="telegraphData"></TimePanel>
-    </a-tab-pane>
-    <a-tab-pane key="jinShi" tab="金十数据"></a-tab-pane>
-  </a-tabs>
+  <div class="market-newsflash-container">
+    <Tabs v-model="activeTab" :tabs="tabItems">
+      <template #default="{ activeTab }">
+        <div v-show="activeTab === 'news'" class="content-panel">
+          <p>这里是新闻内容区。</p>
+        </div>
+        <div v-show="activeTab === 'alerts'" class="content-panel">
+          <p>这里是快讯内容区。</p>
+        </div>
+      </template>
+    </Tabs>
+  </div>
 </template>
 
 <script setup lang="ts">
-import TimePanel from '@components/Common/Timeline.vue'
-import { ref, reactive, onMounted } from 'vue'
-const newsKey = ref('telegraph')
+import { ref } from 'vue'
+import Tabs from '@components/common/Tabs.vue'
 
-const telegraphData = reactive([])
+const activeTab = ref('news')
+const tabItems = ref([
+  { key: 'news', title: '新闻' },
+  { key: 'alerts', title: '快讯' }
+])
 </script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.market-newsflash-container {
+  height: 100%;
+  width: 100%;
+}
+
+.content-panel {
+  height: 100%;
+}
+</style>
