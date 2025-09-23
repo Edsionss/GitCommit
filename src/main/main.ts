09d70a5eeba457c1547a4e2fb9232565f5d0fbea
longhai shen <bigsea1017@gmail.com>
2025-09-23 17:45:33 +0800

    feat(services): 添加爬虫任务执行功能
    在主进程启动时执行一次爬虫任务，用于测试数据抓取逻辑。
    该任务从指定 URL 抓取电报内容，并解析时间、标题、正文及重要性信息。
    引入 executeScrapingTask 方法并传入回调函数处理页面数据提取。

--- src/main/main.ts
+++ src/main/main.ts
@@ -5,6 +5,7 @@ import { registerIpcHandlers } from '@handlers/ipcHandlers'
 import { startWebSocketServer, stopWebSocketServer } from '@services/websocket'
 import { db } from '@features/database'
 import { is } from '@electron-toolkit/utils'
+import { executeScrapingTask } from '@services/puppeteer'
 
 // 创建窗口前的周期函数
 export const beforeCreate = () => {
@@ -69,6 +70,37 @@ export const whenReady = () => {
   if (storedAutoStart && !actualAutoStart) {
     settingsService.setAutoStart(true) // 重新应用设置
   }
+
+  // 测试爬虫任务
+  executeScrapingTask('https://www.cls.cn/telegraph', () => {
+    const result: any[] = []
+    const elements: HTMLElement[] = Array.from(document.querySelectorAll('.telegraph-content-box'))
+
+    elements.forEach((el) => {
+      const timeBox: HTMLElement | null = el.querySelector('.telegraph-time-box')
+      if (!timeBox) return // 如果没有时间盒子，跳过这个元素
+      const contentBox: HTMLElement | null = timeBox.nextElementSibling as HTMLElement
+      if (!contentBox) return // 如果没有内容盒子，跳过这个元素
+      const titleBox: HTMLElement | null = contentBox.querySelector('strong')
+      let newsBox: HTMLElement | null = null
+      if (titleBox) {
+        newsBox = titleBox?.nextSibling as HTMLElement
+      } else {
+        newsBox = contentBox.querySelector('div')
+      }
+      // const news: HTMLElement | null = titleBox?.nextSibling as HTMLElement
+      const isImportant = contentBox.classList.contains('c-de0422')
+      result.push({
+        time: timeBox?.textContent?.trim() || '',
+        content: newsBox?.textContent?.trim() || '',
+        title: titleBox?.textContent?.trim() || '',
+        isImportant
+      })
+    })
+    return result
+  }).then((data) => {
+    console.log('Scraped data:', data)
+  })
 }
 // 应用即将退出的周期函数
 export const willQuit = () => {
